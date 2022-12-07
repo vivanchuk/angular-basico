6349b83ecf8e23c4353f5f66e3ff4eb18e2f5b6a
vivanchuk <xabierbarbazan1996@gmail.com>
2022-12-07 09:38:56 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   templateUrl: 'app.component.html',
+// template: `
+//   <h1>hola de nuevo</h1>
+// `
 })
 export class AppComponent {
-  title = 'bases';
+  
 }
---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-contador',
+    template: `
+    <h1>{{ title }}</h1>
+
+
+    <h3> La base es: <strong> {{ base }}</strong></h3>
+    
+    
+    <button (click)=" acumular(base) "> + {{base}}</button>
+    
+    
+    <span> {{ numero }} </span>    
+    
+    
+    <button (click)=" acumular(-base) "> - {{base}}</button>
+    
+
+
+    `
+})
+
+
+export class ContadorComponent {
+    public title: string = 'contador app';
+    numero: number = 10;
+    base: number = 5;
+  
+    sumar(){
+      this.numero +=1;
+    }
+  
+    restar(){
+      this.numero -=1;
+    }
+  
+    acumular(valor:number){
+      this.numero += valor;
+    }
+}
+
+// despues iriamos al app.module e importariamos contadorcomponent
